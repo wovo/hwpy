ecee4779c8fc8af6515c67e822cbad00d655dd38
Wouter van Ooijen <wouter@voti.nl>
2020-01-05 15:11:17 +0100

    update

---
+++ demo/rapi/sr04-servo.py
@@ -0,0 +1,19 @@
+"""
+Demo for the sr04 ultra-sonic distance sensor
+controlling a servo
+"""
+
+import sys, time
+sys.path.append( "../.." )
+import hwpy
+print( __doc__)
+
+sr04 = hwpy.sr04( hwpy.gpo( 19 ), hwpy.gpi( 26 ) )
+servo = hwpy.servo( hwpy.gpo( 18 ))
+
+while True:
+   cm = sr04.read()
+   if cm < 5: cm = 5
+   if cm > 25: cm = 25
+   servo.write( 1000 + 1000 * ( cm - 5 ) / 25 )
+   time.sleep( 0.2 )
