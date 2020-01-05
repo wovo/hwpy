9e6684d86ddab0600a858bb9060d8218a9f150cb
Wouter van Ooijen <wouter@voti.nl>
2020-01-05 18:05:08 +0100

    update

---
+++ demo/rapi/pcf8574-hw-hd44780.py
@@ -0,0 +1,21 @@
+"""
+Basic HD44780 character LCD interface using pcf8574 and hardware i2c
+"""
+
+import sys
+sys.path.append( "../.." )
+import hwpy
+print( __doc__)
+
+
+i2c = hwpy.i2c_hardware()
+port = hwpy.pcf8574(i2c, 0x07)
+lcd = hwpy.hd44780.from_pcf8574(pcf, hwpy.xy(16,2))
+
+lcd.write( "\fHello world" )
+n = 0
+while True:
+    n = ( n + 1 ) % 16
+    lcd.write( "\t0001" + n * "=" )
+    hwpy.wait_ms(200)
+
