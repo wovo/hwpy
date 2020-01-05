c9bf5a4a6e2f8850115b4e12c7666e7aa10d2c15
Wouter van Ooijen <wouter@voti.nl>
2020-01-05 18:10:36 +0100

    update

--- demo/rapi/pcf8574-hw-hd44780.py
+++ demo/rapi/pcf8574-hw-hd44780.py
@@ -15,7 +15,7 @@ lcd = hwpy.hd44780.from_pcf8574(pcf, hwpy.xy(16,2))
 lcd.write( "\fHello world" )
 n = 0
 while True:
-    n = ( n + 1 ) % 16
-    lcd.write( "\t0001" + n * "=" )
+    n = ( n + 1 ) % 17
+    lcd.write( "\t0001" + ( n * "=" ) + ( ( 16 - n ) * " " ))
     hwpy.wait_ms(200)
 
