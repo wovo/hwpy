e4e9fc1de4ced613cf07ebf5fbd6a934665571af
Wouter van Ooijen <wouter@voti.nl>
2020-01-05 15:31:58 +0100

    update

---
+++ demo/rapi/pcf8574a_blink_one_hw.py
@@ -0,0 +1,13 @@
+"""
+Blink on a single LED connected to a pcf8574a using hardware i2c
+"""
+
+import sys
+sys.path.append( "../.." )
+import hwpy
+print( __doc__)
+
+i2c = hwpy.i2c_hardware( 1 )
+chip = hwpy.pcf8574( i2c )
+ 
+hwpy.blink( chip.pins[ 0 ], 0.100 )
