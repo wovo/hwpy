e305b92e2a2ea4edd31cf7ed809dead7c7df8cc7
wovo <wouter@voti.nl>
2020-01-07 16:24:29 +0100

    update

---
+++ demo/db103-server/mpu6050.py
@@ -0,0 +1,28 @@
+"""
+Show the values read from an mpu6050
+"""
+
+import sys, time
+sys.path.append( "../.." )
+import hwpy
+print( __doc__)
+
+def mpu6050_demo(mpu: hwpy.mpu6050, abort_when=lambda: False):
+    while not abort_when():
+        gyro = mpu.gyroscopes()
+        accel = mpu.acceleration()
+        print("%10d %10d %10d %10d %10d %10d %10d" % (
+            mpu.temperature(),
+            gyro.x,
+            gyro.y,
+            gyro.z,
+            accel.x,
+            accel.y,
+            accel.z
+        ))
+
+
+if __name__ == '__main__':
+    i2c = hwpy.i2c_from_scl_sda(hwpy.gpoc(hwpy.db103.scl), hwpy.gpoc(hwpy.db103.sda))
+    chip = hwpy.mpu6050(i2c)
+    mpu6050_demo(chip)
